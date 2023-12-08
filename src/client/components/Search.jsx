0a3816c6c5ca52a17d59700a2f4bab7b0308e659
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-12-07 19:03:26 -0500

    working on search component

---
+++ src/client/components/Search.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import AllItems from "./Allitems";
+import axios from "axios";
+
+function ItemSearch() {
+  const [items, setItems] = useState([]);
+  const [query, setQuery ] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await 
+      }
+    }
+  }
+
+  )
+}
